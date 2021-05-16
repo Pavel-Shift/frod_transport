b356b392d26ac02a5e817a7b3e2ec8c1d925cd6b
Pavel Shift <shift.pavel@mail.ru>
2021-05-16 18:45:02 +0400

    Исправление ошибок, доработка опознания населенного пункта

--- analize.py
+++ analize.py
@@ -4,6 +4,7 @@
 # при наличии запросов сотрудники уезжают, с целью вернуться для увеличения пробегов
 
 from datetime import datetime
+import string
 
 # чтение файла в список и подсчёт строк
 f_in = open('zno.csv', 'r')
@@ -31,7 +32,11 @@ f_out = open('zno+analize.csv', 'w')
 # проходимся по сотрудникам в алфавитном порядке
 print('ФИО = заявок у сотрудника')
 for fio in fio_mn_sort:
-    # выбираем запросы у каждого сотрудника и исчитаем их
+    # выбираем запросы у каждого сотрудника и считаем их
+# тестирование
+#    if fio!='Анисимов Алексей Викторович':
+#        break
+# тестирование
     j = 0
     zno = []
     for line in base:
@@ -39,8 +44,6 @@ for fio in fio_mn_sort:
         if line_sp[5] == fio:
             j = j + 1
             zno.append(line)
-    print(fio + ' = ' + str(j))
-
     # проход по запросам одного сотрудника
     city = ''
     zno_c = ''
@@ -50,37 +53,41 @@ for fio in fio_mn_sort:
     zno_skip = ''
     for line in zno:
         line_sp = line.split(';')
+        if len(line_sp[7])>0:
+            while not (line_sp[7][0].isupper()) and len(line_sp[7])>1:
+                line_sp[7] = line_sp[7][1:]
         # ФИО, номер запроса, дата назначенмя, дата выполнения, населенный пункт
         text = line_sp[5] + ';' + line_sp[2]+';'+line_sp[3]+';'+line_sp[4]+';'+line_sp[7]
+        # Первый запрос у сотрудника, предыдуший населенный пункт неизвестен
         if city =='':
             city = line_sp[7]
             zno_c = line_sp[2]
             close = line_sp[4]
         else:
+            # Населенный пункт совпадает с предыдущим запросом или сотрудник поставил пробег 0
+            y = 0
+            zno_skip = ''
             if city == line_sp[7] or int(line_sp[9]) == 0:
                 text=text+';нет перемещения'
                 city = line_sp[7]
                 zno_c = line_sp[2]
                 close = line_sp[4]
-#                print(city + ' ' + zno_c + ' ' + close)
+            # населенный пункт не совпадает с предыдущим запросом
             else:
-#                print('есть перемещение')
                 text = text + ';есть перемещение'
-                y = 0
+                # считаем количество невыполненых запросов в населенном пункте
                 for line_2 in zno:
                     line_2_sp = line_2.split(';')
-                    zno_skip = ''
                     if ( (datetime.strptime(line_2_sp[3], '%d.%m.%Y %H:%M') <  datetime.strptime(close, '%d.%m.%Y %H:%M')) and (datetime.strptime(line_2_sp[4], '%d.%m.%Y %H:%M') > datetime.strptime(close, '%d.%m.%Y %H:%M')) and city==line_2_sp[7]):
-#                        print(line_2_sp[2])
                         y = y + 1
-                        zno_skip = zno_skip + line_2_sp[2]
+                        zno_skip = zno_skip + ' ' +line_2_sp[2]
                 if y > 0:
+                    # считаем количество случаев когда сотрудник уезжал из населеннного пункта не выполняя запросы
                     x = x +1
                 city = line_sp[7]
                 zno_c = line_sp[2]
                 close = line_sp[4]
-#                print(city+' '+zno_c+' '+close)
         f_out.write(text+';'+str(x)+';'+str(y)+';'+zno_skip+'\n')
-#    print(fio + ' = ' + str(j) + ' ' + str(x))
+    print(fio + ' = ' + str(j) + ' кейсов = ' + str(x))
 f_out.close()
 print('Остановка')
