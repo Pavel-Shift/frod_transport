8208a52947657ff59ff7b6b0c9095a3a133f58cf
Pavel Shift <shift.pavel@mail.ru>
2021-05-16 16:05:32 +0400

    Добавление комментариев

--- analize.py
+++ analize.py
@@ -1,6 +1,11 @@
+# Анализ выездов по запросам
+# Цель выявить повторные поездки в населенные пункты
+# Предположительно сотрудники не выполняют взе запросы из населенных пунктов,
+# при наличии запросов сотрудники уезжают, с целью вернуться для увеличения пробегов
 
 from datetime import datetime
-# чтение файла в список
+
+# чтение файла в список и подсчёт строк
 f_in = open('zno.csv', 'r')
 i = 0
 base = []
@@ -8,17 +13,25 @@ for line in f_in:
     base.append(line)
     i = i + 1
 f_in.close()
-print('Строк = ' + str(i))
+print('Строк в файле = ' + str(i))
 
+# добавление фио сотрудников в множество и подсчёт сотрудников
 fio_mn = set()
 for line in base:
     line_sp = line.split(';')
     fio_mn.add(line_sp[5])
+print('Сотрудников = ' + str(len(fio_mn)))
 
-print('ФИО = ' + str(len(fio_mn)))
+# сортировка заявок
 fio_mn_sort  =sorted(fio_mn)
+
+
 f_out = open('zno+analize.csv', 'w')
+
+# проходимся по сотрудникам в алфавитном порядке
+print('ФИО = заявок у сотрудника')
 for fio in fio_mn_sort:
+    # выбираем запросы у каждого сотрудника и исчитаем их
     j = 0
     zno = []
     for line in base:
@@ -27,6 +40,8 @@ for fio in fio_mn_sort:
             j = j + 1
             zno.append(line)
     print(fio + ' = ' + str(j))
+
+    # проход по запросам одного сотрудника
     city = ''
     zno_c = ''
     close = ''
@@ -35,12 +50,12 @@ for fio in fio_mn_sort:
     zno_skip = ''
     for line in zno:
         line_sp = line.split(';')
+        # ФИО, номер запроса, дата назначенмя, дата выполнения, населенный пункт
         text = line_sp[5] + ';' + line_sp[2]+';'+line_sp[3]+';'+line_sp[4]+';'+line_sp[7]
         if city =='':
             city = line_sp[7]
             zno_c = line_sp[2]
             close = line_sp[4]
-#            print(city+' '+zno_c+' '+close)
         else:
             if city == line_sp[7] or int(line_sp[9]) == 0:
                 text=text+';нет перемещения'
